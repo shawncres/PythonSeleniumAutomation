d6ee5d0d771c7521aa1e965385c06a40e86bc28f
shawncres <90995873+shawncres@users.noreply.github.com>
2022-05-02 13:30:09 -0600

    Update Test code methods.py

--- Test code methods.py
+++ Test code methods.py
@@ -38,40 +38,112 @@
 ##a.listconst([100,200,400])
 
 
-
-
-class A:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-
-
-    def operate(self):
-        return self.x * self.y
-
-
-class Ba(A):
-    def __init__(self,x,y,z):
-        super().__init__(x,y)
-        self.z = z
-
-    def newoperate(self):
-        return self.operate() + self.z
-
-
-j = Ba(3,4,5)
-
-print(j.newoperate())
-
-
-
-
-
-
-
+##test 2
+##
+##class A:
+##    def __init__(self,x,y):
+##        self.x = x
+##        self.y = y
+##
+##
+##    def operate(self):
+##        return self.x * self.y
+##
+##
+##class Ba(A):
+##    def __init__(self,x,y,z):
+##        super().__init__(x,y)
+##        self.z = z
+##
+##    def newoperate(self):
+##        return self.operate() + self.z
+##
+##
+##j = Ba(3,4,5)
+##
+##print(j.newoperate())
+##
+##
+##
+##
 
 
 
+## test 3
+##
+##class maintest:
+##    def __init__(self):
+##        self.x = 50
+##
+##
+##
+##class subtesta(maintest):
+##    def __init__(self):
+##        super().__init__()
+##
+##class subtestb(maintest):
+##    def __init__(self):
+##        super().__init__()
+##        self.a = 100
+##
+##    def multiply(self):
+##        return  self.x * self.a
+##
+##class subtestc(maintest):
+##    def __init__(self,value):
+##        super().__init__()
+##        self.value = value
+##
+##    def listcomp(self):
+##        newlist = [self.x * k for k in self.value]
+##        print(newlist)
+##
+##
+##    
+##
+##j = subtestc([1,5,10])
+##
+##
+###print(j.multiply())
+##
+##j.listcomp()
+##        
+##
+##j = subtestb()
+##
+##print(j.multiply())
+##
+
+
+class human:
+    def __init__(self):
+        self.height = 50
+        self.weight = 100
+
+    def greeting(self):
+        print('Hi I\'m a human being')
+
+    def stats(self):
+        print(f'I am {self.height}cm tall and weight {self.weight}kg')
+
+class child(human):
+    def __init__(self):
+        super().__init__()
+        print('I am a child')
+
+
+
+
+
+john = child()
+
+
+john.greeting()
+
+john.stats()
+
+        
+        
 
 
 
