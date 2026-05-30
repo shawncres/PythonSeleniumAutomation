cd3825425ce71d7599445cc9102f3bc5a2ff56c8
shawncres <90995873+shawncres@users.noreply.github.com>
2026-05-30 11:07:56 -0400

    chore: remove legacy HM locator file

--- Locators_HM.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-# NOTE: Company and project names sanitized for public repository (original: Attabotics / Hivemind)
-# Locators updated for RoboticsERP (sanitized version of Hivemind)
-
-# Example locators (sanitized):
-ri_page = 'https://robotinc-roboticserp-test.example.com/'
-# ... other locators would be updated here ...
